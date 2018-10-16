65716f0092d5f495f75f9d0616aef4e41f069f4f
moneer-alasmar <kiraalasmar@gmail.com>
2018-10-16 16:53:46 -0700

    completed component creation and insertion to DOM

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,10 +1,13 @@
 // IMPORTING REACT
 import React from 'react';
 
+// IMPORTING REACT DOM
+import ReactDOM from 'react-dom';
+
 // CREATE A NEW COMPONENT. THIS COMPONENT SHOULD PRODUCE HTML
-const App = function() {
+const App = () => {
   return <div>Hi</div>;
 }
 
 // TAKE COMPONENT'S GENERATED HTML AND PUT IT ON THE PAGE(IN THE DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
