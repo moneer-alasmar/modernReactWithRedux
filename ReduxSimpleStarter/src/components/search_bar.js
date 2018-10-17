36cf67230de068a3852a16d68934f02fc3f99450
moneer-alasmar <kiraalasmar@gmail.com>
2018-10-16 19:37:03 -0700

    converted component to class based

---
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// FUNCTION BASEDC COMPONENT
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+// CLASS BASED COMPONENT
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -4,11 +4,18 @@ import React from 'react';
 // IMPORTING REACT DOM
 import ReactDOM from 'react-dom';
 
+// IMPORT SEARCHBAR
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDIkfflCWAPULhAZ4kKl2EMR0AyuEuCfMU';
 
 // CREATE A NEW COMPONENT. THIS COMPONENT SHOULD PRODUCE HTML
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // TAKE COMPONENT'S GENERATED HTML AND PUT IT ON THE PAGE(IN THE DOM)
