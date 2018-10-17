6ac738a2397040e71063fa959e1c8089013edb2e
moneer-alasmar <kiraalasmar@gmail.com>
2018-10-17 09:54:01 -0700

    converted app function component to class based component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,5 +1,5 @@
 // IMPORTING REACT
-import React from 'react';
+import React, { Component } from 'react';
 
 // IMPORTING REACT DOM
 import ReactDOM from 'react-dom';
@@ -10,15 +10,29 @@ import SearchBar from './components/search_bar';
 // IMPORT CONFIG
 import { myConfig } from './components/config';
 
+// IMPORT YOUTUBE-API-SEARCH
+import YTSearch from 'youtube-api-search';
+
 const API_KEY = myConfig.API_KEY;
 
 // CREATE A NEW COMPONENT. THIS COMPONENT SHOULD PRODUCE HTML
-const App = () => {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'cheetahs'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render () {
+    return (
     <div>
       <SearchBar />
     </div>
-  );
+    );
+  }
 }
 
 // TAKE COMPONENT'S GENERATED HTML AND PUT IT ON THE PAGE(IN THE DOM)
