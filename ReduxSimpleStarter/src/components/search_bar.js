5cd19b48e5922447c17ccf6111a157ce692a794d
moneer-alasmar <kiraalasmar@gmail.com>
2018-10-16 19:50:25 -0700

    completed event handle logic for search_bar

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -8,8 +8,13 @@ import React, { Component } from 'react';
 // CLASS BASED COMPONENT
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
+
+  // EVENT HANDLER
+  // handleInputChange(event) {
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
