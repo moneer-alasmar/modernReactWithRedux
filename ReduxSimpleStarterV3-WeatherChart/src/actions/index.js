94a3e05bba78f4680734a7b2318b618a6cdbe516
moneer-alasmar <kiraalasmar@gmail.com>
2018-10-25 12:23:47 -0700

    added axios for our AJAX requests

--- ReduxSimpleStarterV3-WeatherChart/src/actions/index.js
+++ ReduxSimpleStarterV3-WeatherChart/src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = '8b77c6939fe88418a4dc3a801cc1cc0b';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}, us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
