ea31eb9c1ccf4817eba47ea073e65a4278dfb304
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-03 14:09:42 -0700

    improved routing for posts list on index

--- ReduxSimpleStarterV4-PostsCrud/src/components/posts_show.js
+++ ReduxSimpleStarterV4-PostsCrud/src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
   componentDidMount() {
@@ -9,9 +10,19 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if(!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <Link className="btn btn-primary" to="/">Back To Index</Link>
+        <hr></hr>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
