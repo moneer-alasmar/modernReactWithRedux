40b837412e8ac194c23c5ee35325de0257871573
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-18 12:35:31 -0800

    created CommentDetail component

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -1,24 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            JoSchmo
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
