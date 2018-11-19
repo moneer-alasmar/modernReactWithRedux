7ee4d5c8b55813f5a819cca7d14ff542d9e86017
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-19 10:14:11 -0800

    began props review

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -5,10 +5,10 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Bissy" />
-      <CommentDetail author="Raja" />
-      <CommentDetail author="Naji" />
-      <CommentDetail author="Naia" />
+      <CommentDetail author="Bissy" timeAgo="Today at 4:30PM" />
+      <CommentDetail author="Raja" timeAgo="Today at 2:30PM" />
+      <CommentDetail author="Naji" timeAgo="Today at 1:30PM" />
+      <CommentDetail author="Naia" timeAgo="Today at 12:30PM" />
     </div>
   );
 };
