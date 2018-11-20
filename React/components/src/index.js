a7782d31dd525f7f1214b770c95cbf9b703e9443
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-19 17:55:13 -0800

    created ApprovalCard component as a parent to Comment Detail

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -2,34 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Bissy"
-        timeAgo="Today at 4:30PM"
-        comment="Wow"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Raja"
-        timeAgo="Today at 2:30PM"
-        comment="No way!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Naji"
-        timeAgo="Today at 1:30PM"
-        comment="Savage!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Naia"
-        timeAgo="Today at 12:30PM"
-        comment="Whoops"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Bissy"
+          timeAgo="Today at 4:30PM"
+          comment="Wow"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Raja"
+          timeAgo="Today at 2:30PM"
+          comment="No way!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Naji"
+          timeAgo="Today at 1:30PM"
+          comment="Savage!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Naia"
+          timeAgo="Today at 12:30PM"
+          comment="Whoops"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
