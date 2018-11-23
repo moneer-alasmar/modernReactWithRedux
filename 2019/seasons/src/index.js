d5b42b0fafa1c13f1bb72dc4bb684c7fe4b0e687
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-23 10:06:52 -0800

    implemented state into seasons app

--- 2019/seasons/src/index.js
+++ 2019/seasons/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null
+    };
+  }
   render() {
     // Geolocation API takes in 2 call backs, the first for successful and the second for failure.
     window.navigator.geolocation.getCurrentPosition(
