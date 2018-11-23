d65a8e9424d757c232cff574f236819282f3b92e
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-23 15:04:02 -0800

    updated constructor method of index

--- 2019/seasons/src/index.js
+++ 2019/seasons/src/index.js
@@ -5,20 +5,24 @@ import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    // THIS IS THE ONLY TIME WE DO A DIRECT ASSIGNMENT TO THIS.STATE
     this.state = {
       lat: null
     };
-  }
-  render() {
+
     // Geolocation API takes in 2 call backs, the first for successful and the second for failure.
     window.navigator.geolocation.getCurrentPosition(
       // Success Callback
-      position => console.log(position),
+      position => {
+        // ALWAYS USE SET STATE TO UPDATE STATE
+        this.setState({ lat: position.coords.latitude });
+      },
       // Failure Callback
       err => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
