fc7f2528fbd9bff91b61cdf490876303c82dc932
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-24 12:54:46 -0800

    implemented seasondisplay component

--- 2019/seasons/src/SeasonDisplay.js
+++ 2019/seasons/src/SeasonDisplay.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SeasonDisplay = props => {
-  return <div>Season Display</div>;
+  return <div>Season Display{props.lat}</div>;
 };
 
 export default SeasonDisplay;
