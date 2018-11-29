15541f8092c85dd338da8190684f321dc1f080c3
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-29 13:00:41 -0800

    changed from promise based axios to async await method

--- 2019/pics/src/components/App.js
+++ 2019/pics/src/components/App.js
@@ -3,14 +3,15 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID e21f336b8b140fb26c7125f80d7db79c0bf91e48c4e81d703db6275bfdcb3780"
       }
     });
+    console.log(response.data.results);
   }
   render() {
     return (
