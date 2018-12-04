d22653b49c546ee875f1e4c8e34feee0df538716
moneer-alasmar <kiraalasmar@gmail.com>
2018-12-04 09:36:14 -0800

    created custom axios client

--- 2019/pics/src/components/App.js
+++ 2019/pics/src/components/App.js
@@ -1,22 +1,20 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID e21f336b8b140fb26c7125f80d7db79c0bf91e48c4e81d703db6275bfdcb3780"
-      }
+  state = { images: [] };
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
     });
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
