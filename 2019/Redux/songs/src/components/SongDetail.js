2a317c5660e494a27c51c74f70d37b047edce382
moneer-alasmar <kiraalasmar@gmail.com>
2019-01-05 14:19:18 -0800

    created song detail component and wired it up to connect

---
+++ 2019/Redux/songs/src/components/SongDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = props => {
+  console.log(props);
+  return <div>{props.song.title}</div>;
+};
+
+const mapStateToProps = state => {
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
